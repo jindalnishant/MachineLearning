01384bba77dc33ab2b038c4801f7cf22d3456a8f
jindalnishant <47566866+jindalnishant@users.noreply.github.com>
2020-05-07 17:57:02 +0530

    Add files via upload

---
+++ 11.Linear Regression/HousePrice_Pycharm.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+house = pd.read_csv("D:\\02_PROGRAMS\\104.MACHINE_LEARNING\\11.Linear Regression\\housepricedata.csv")
+house.head()
+
+a=1
+print(a)
+print(house.columns)
+print(house.shape)
